da93a0d8c12202fc794db2ad653aaeb76fe7c751
YPuest <56340897+YPuest@users.noreply.github.com>
2025-03-10 08:58:20 +0100

    Revert "Sapi prod 3"

--- src/main/java/com/example/shopapi/domain/repository/OrderItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.shopapi.domain.repository;
-
-import com.example.shopapi.domain.model.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-    boolean existsByProductId(Long productId);
-}
